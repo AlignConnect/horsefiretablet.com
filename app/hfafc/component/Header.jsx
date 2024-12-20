93814e503e9803a9e479003c0d972c0c48ea9d6d
Align <devlopersalignconnect@gmail.com>
2024-12-20 10:38:53 +0530

    f

---
+++ app/hfafc/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      adasd
+    </div>
+  )
+}
+
+export default Header
--- app/hfafc/page.jsx
+++ app/hfafc/page.jsx
@@ -1,11 +1,16 @@
+import dynamic from 'next/dynamic'
 import React from 'react'
+
+
+const Header = dynamic(() => import('@/app/hfafc/component/Header'))
 
 const page = () => {
   return (
     <div>
-      
-        hfarc
 
+      <Header />
+
+      
     </div>
   )
 }
