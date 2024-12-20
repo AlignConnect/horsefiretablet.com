741b22481c3de62ebb128eeed67db4e2a675f831
Align <devlopersalignconnect@gmail.com>
2024-12-20 11:17:21 +0530

    headeradded

--- app/hfafc/component/Header.jsx
+++ app/hfafc/component/Header.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div>
-      adasd
-    </div>
-  )
+    return (
+        <div className="bg-[url(https://imagedelivery.net/aacnHGAqlUDhaplA3bnkbA/hfarc2/public)] bg-cover text-white">
+
+            <div className='grid w-[70%] mx-auto grid-cols-2 items-center '>
+
+                <div className='fontAnton'>
+                    <h1 className='text-4xl '>#1 Selling Sex Booster in India</h1>
+
+
+                    <div className='font-bold text-7xl my-5 bg-gradient-to-b to-[#fee600] from-[#ef3803] text-transparent bg-clip-text'>
+                        Horse Fire Tablet
+                    </div>
+
+                    <div className='text-6xl font-[700] fontNoto tracking-wider leading-none'>
+                        <p>
+                            Get Big Penis,
+                        </p>
+                        <p>
+                            Do Last Long Sex
+                        </p>
+                    </div>
+
+                    <div className='text-4xl fontNoto my-5 leading-tight'>
+                        "सेक्स टाइम और सख्ती बढ़ाएं <br /> 100% गारंटी के साथ"
+                    </div>
+
+
+                </div>
+
+
+
+
+
+                <div>
+
+                    <div>
+                        <img src="https://imagedelivery.net/aacnHGAqlUDhaplA3bnkbA/hfarc1/public" alt="dssdsd" loading='lazy' />
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default Header
--- app/hfafc/page.jsx
+++ app/hfafc/page.jsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
+import './Home.css'
 
 
 const Header = dynamic(() => import('@/app/hfafc/component/Header'))
